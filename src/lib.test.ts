73c4a4bb1c255191b57ef81a2e2bd4b2222a4fab
Ben Wilhelm <benjamin.m.wilhelm@gmail.com>
2023-02-25 13:20:20 -0600

    calculateResults tested

--- src/lib.test.ts
+++ src/lib.test.ts
@@ -1,7 +1,57 @@
-import { add } from './lib';
+import { Run, calculateResults } from './lib';
 
-describe('lib', () => {
-  it('smoke test', () => {
-    expect(add(1, 2)).toEqual(4);
+describe('calculateResults()', () => {
+  it('should transpose matrix', () => {
+    const runs = runFactory();
+    const results = calculateResults(runs);
+
+    expect(results).toEqual([
+      {
+        ok: true,
+        message: 'Comparison 1',
+        runs: [
+          { name: 'Run 1', value: 1 },
+          { name: 'Run 2', value: 1 },
+        ],
+      },
+      {
+        ok: false,
+        message: 'Comparison 2',
+        runs: [
+          { name: 'Run 1', value: 'foo' },
+          { name: 'Run 2', value: 'bar' },
+        ],
+      },
+      {
+        ok: true,
+        message: 'Comparison 3',
+        runs: [
+          { name: 'Run 1', value: { bif: 'baz' } },
+          { name: 'Run 2', value: { bif: 'baz' } },
+        ],
+      },
+    ]);
   });
 });
+
+const runFactory = (): Run[] => {
+  return [
+    {
+      name: 'Run 1',
+      checks: [
+        { message: 'Comparison 1', value: 1 },
+        { message: 'Comparison 2', value: 'foo' },
+        { message: 'Comparison 3', value: { bif: 'baz' } },
+      ],
+    },
+
+    {
+      name: 'Run 2',
+      checks: [
+        { message: 'Comparison 1', value: 1 },
+        { message: 'Comparison 2', value: 'bar' },
+        { message: 'Comparison 3', value: { bif: 'baz' } },
+      ],
+    },
+  ];
+};
--- src/lib.ts
+++ src/lib.ts
@@ -1,3 +1,43 @@
-export const add = (x: number, y: number) => {
-  return x + y;
+import assert from 'node:assert';
+
+interface Comparison {
+  value: unknown;
+  message: string;
+}
+export interface Run {
+  name: string;
+  checks: Comparison[];
+}
+
+interface Result {
+  ok: boolean;
+  message: string;
+  runs: { name: string; value: unknown }[];
+}
+
+export const calculateResults = (runs: Run[]): Result[] => {
+  return runs[0].checks.map((check, checkIdx) => {
+    const runResults = runs.map((run, runIdx) => ({
+      value: run.checks[checkIdx].value,
+      name: runs[runIdx].name,
+    }));
+
+    const ok = runResults.reduce((isOk, check, checkIdx) => {
+      if (!isOk) return false;
+      if (checkIdx === 0) return true;
+      const previousCheck = runResults[checkIdx - 1];
+      try {
+        assert.deepEqual(check.value, previousCheck.value);
+        return true;
+      } catch (err) {
+        return false;
+      }
+    }, true);
+
+    return {
+      ok,
+      message: check.message,
+      runs: runResults,
+    };
+  });
 };
