36d501a8d01ff1b0b051776b87410d7c76cbc3e9
Ben Wilhelm <benjamin.m.wilhelm@gmail.com>
2023-02-27 09:21:24 -0600

    custom comparator

--- src/lib.test.ts
+++ src/lib.test.ts
@@ -1,4 +1,82 @@
-import { Run, calculateResults } from './lib';
+import { isEqual } from 'lodash';
+import {
+  ResultCb,
+  TestFunc,
+  Comparator,
+  Factor,
+  Run,
+  calculateResults,
+  runComparison,
+} from './lib';
+
+describe('runComparison()', () => {
+  it('does the needful', () => {
+    const { factors, testFn, resultsCb } = callbackFactory();
+
+    runComparison(factors, testFn, resultsCb);
+
+    expect(testFn).toHaveBeenCalledTimes(2);
+    expect(testFn.mock.calls[0][0]).toBe(factors[0]);
+    expect(testFn.mock.calls[1][0]).toBe(factors[1]);
+
+    expect(resultsCb).toHaveBeenCalledTimes(1);
+    expect(resultsCb).toHaveBeenCalledWith([
+      {
+        ok: false,
+        message: 'foo comparison',
+        runs: [
+          { name: 'Factor 1', value: 'bar' },
+          { name: 'Factor 2', value: 'bif' },
+        ],
+      },
+      {
+        ok: true,
+        message: 'hard-coded',
+        runs: [
+          { name: 'Factor 1', value: 'x' },
+          { name: 'Factor 2', value: 'x' },
+        ],
+      },
+    ]);
+  });
+  it('accepts a custom comparator', () => {
+    const withinOne: Comparator = (a, b) =>
+      Math.abs((a as number) - (b as number)) <= 1;
+    const customFactors: Factor[] = [
+      { name: 'Factor 1', variables: { x: 1, y: 1 } },
+      { name: 'Factor 2', variables: { x: 2, y: 3 } },
+    ];
+    const testFnImp: TestFunc = (factor, compare) => {
+      compare(factor.variables.x, 'compare x', withinOne);
+      compare(factor.variables.y, 'compare y', withinOne);
+    };
+    const { factors, testFn, resultsCb } = callbackFactory({
+      factors: customFactors,
+      testFnImp,
+    });
+
+    runComparison(factors, testFn, resultsCb);
+
+    expect(resultsCb).toHaveBeenCalledWith([
+      {
+        ok: true,
+        message: 'compare x',
+        runs: [
+          { name: 'Factor 1', value: 1 },
+          { name: 'Factor 2', value: 2 },
+        ],
+      },
+      {
+        ok: false,
+        message: 'compare y',
+        runs: [
+          { name: 'Factor 1', value: 1 },
+          { name: 'Factor 2', value: 3 },
+        ],
+      },
+    ]);
+  });
+});
 
 describe('calculateResults()', () => {
   it('should transpose matrix', () => {
@@ -34,23 +112,52 @@ describe('calculateResults()', () => {
   });
 });
 
+type CallbackOptions = {
+  factors: Factor[];
+  testFnImp: TestFunc;
+  resultsCbImp: ResultCb;
+};
+const callbackFactory = (userOptions: Partial<CallbackOptions> = {}) => {
+  const defaults: CallbackOptions = {
+    factors: factorFactory(),
+    testFnImp: (factor, compare) => {
+      compare(factor.variables.foo, 'foo comparison');
+      compare('x', 'hard-coded');
+    },
+    resultsCbImp: (results) => {},
+  };
+
+  const options = { ...defaults, ...userOptions };
+  const testFn: jest.Mock<TestFunc> = jest
+    .fn()
+    .mockImplementation(options.testFnImp);
+  const resultsCb = jest.fn().mockImplementation(options.resultsCbImp);
+
+  return { factors: options.factors, testFn, resultsCb };
+};
+
+const factorFactory = (): Factor[] => [
+  { name: 'Factor 1', variables: { foo: 'bar' } },
+  { name: 'Factor 2', variables: { foo: 'bif' } },
+];
+
 const runFactory = (): Run[] => {
   return [
     {
       name: 'Run 1',
       checks: [
-        { message: 'Comparison 1', value: 1 },
-        { message: 'Comparison 2', value: 'foo' },
-        { message: 'Comparison 3', value: { bif: 'baz' } },
+        { message: 'Comparison 1', value: 1, comparator: isEqual },
+        { message: 'Comparison 2', value: 'foo', comparator: isEqual },
+        { message: 'Comparison 3', value: { bif: 'baz' }, comparator: isEqual },
       ],
     },
 
     {
       name: 'Run 2',
       checks: [
-        { message: 'Comparison 1', value: 1 },
-        { message: 'Comparison 2', value: 'bar' },
-        { message: 'Comparison 3', value: { bif: 'baz' } },
+        { message: 'Comparison 1', value: 1, comparator: isEqual },
+        { message: 'Comparison 2', value: 'bar', comparator: isEqual },
+        { message: 'Comparison 3', value: { bif: 'baz' }, comparator: isEqual },
       ],
     },
   ];
--- src/lib.ts
+++ src/lib.ts
@@ -1,8 +1,8 @@
-import assert from 'node:assert';
-
+import { isEqual } from 'lodash';
 interface Comparison {
   value: unknown;
   message: string;
+  comparator: Comparator;
 }
 export interface Run {
   name: string;
@@ -15,8 +15,53 @@ interface Result {
   runs: { name: string; value: unknown }[];
 }
 
+export interface Factor {
+  name: string;
+  variables: Record<string, any>;
+}
+
+export type Comparator = (a: unknown, b: unknown) => boolean;
+
+export type TestFunc = (factor: Factor, compare: CompareFunc) => void;
+export type ResultCb = (results: Result[]) => void;
+
+type CompareFunc = (
+  value: unknown,
+  message: string,
+  comparator?: Comparator
+) => void;
+
+export const runComparison = (
+  factors: Factor[],
+  testFn: TestFunc,
+  resultCb: ResultCb
+) => {
+  const runs: Run[] = [];
+  factors.forEach((factor) => {
+    const run: Run = {
+      name: factor.name,
+      checks: [],
+    };
+    runs.push(run);
+    const compare: CompareFunc = (
+      value: any,
+      message: string,
+      comp?: Comparator
+    ) => {
+      const comparator = comp || isEqual;
+      run.checks.push({ value, message, comparator });
+    };
+    testFn(factor, compare);
+  });
+
+  const results = calculateResults(runs);
+  resultCb(results);
+};
+
 export const calculateResults = (runs: Run[]): Result[] => {
   return runs[0].checks.map((check, checkIdx) => {
+    const { comparator } = check;
+
     const runResults = runs.map((run, runIdx) => ({
       value: run.checks[checkIdx].value,
       name: runs[runIdx].name,
@@ -26,12 +71,7 @@ export const calculateResults = (runs: Run[]): Result[] => {
       if (!isOk) return false;
       if (checkIdx === 0) return true;
       const previousCheck = runResults[checkIdx - 1];
-      try {
-        assert.deepEqual(check.value, previousCheck.value);
-        return true;
-      } catch (err) {
-        return false;
-      }
+      return comparator(check.value, previousCheck.value);
     }, true);
 
     return {
